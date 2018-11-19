98007fb5c099cf3afb0d98dddb7f2a940f94ab64
Francisco Olivé <franolive97@hotmail.com>
2018-11-19 13:35:46 -0300

    .

---
+++ WebContent/login/index.controller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Login.IndexController', Controller);
+
+    function Controller($location, AuthenticationService) {
+        var vm = this;
+
+        vm.login = login;
+
+        initController();
+
+        function initController() {
+            // reset login status
+            AuthenticationService.Logout();
+        };
+
+        function login() {
+            vm.loading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (result) {
+                if (result === true) {
+                    $location.path('/');
+                } else {
+                    vm.error = 'El email y la contraseña son incorrectos.';
+                    vm.loading = false;
+                }
+            });
+        };
+    }
+
+})();
